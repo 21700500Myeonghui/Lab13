43a0daf89c5a6216069db3b92bec30c4bc6e2795
21700500Myeonghui <21700500@handong.edu>
2018-05-01 00:09:20 +0900

    Prob6

--- src/main/java/edu/handong/csee/java/lab13/prob2/Prob2.java
+++ src/main/java/edu/handong/csee/java/lab13/prob2/Prob2.java
@@ -8,9 +8,9 @@ public class Prob2//The declaration of class Prob2
 		// TODO Auto-generated method stub
 
 		Book book1=new Book("Simple Book");//Instantiate of an Book object, calling constructor, value of parameter is "Simple Book" at this time
-		Science book2=new Science("Hello Physics!","ScienceWorld");//Instantiate of an Science object, calling constructor, value of parameters are "Hello Physics!" and "ScienceWorld" at this time
-		History book3=new History("What Is history?", "E.H.Carr");//Instantiate of an History object, calling constructor, value of parameters are "What Is history?" and "E.H.Carr" at this time
-		History book4=new History("The South Korea", "Judis");//Instantiate of an History object, calling constructor, value of parameters are "The South Korea" and "Judis" at this time
+		Science book2=new Science("Hello Physics!","ScienceWorld");//Instantiate of an Science object, calling constructor, values of parameter are "Hello Physics!" and "ScienceWorld" at this time
+		History book3=new History("What Is history?", "E.H.Carr");//Instantiate of an History object, calling constructor, values of parameter are "What Is history?" and "E.H.Carr" at this time
+		History book4=new History("The South Korea", "Judis");//Instantiate of an History object, calling constructor, values of parameter are "The South Korea" and "Judis" at this time
 		
 		book1.display();//calling method dispaly() in book1
 		book2.display();//calling method dispaly() in book2
